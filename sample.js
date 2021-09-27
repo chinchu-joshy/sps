84457bfdf8e766620cfbcbf3d516caab7657a119
chinchu-joshy <66483618+chinchu-joshy@users.noreply.github.com>
2021-09-27 21:57:27 +0530

    first commit

---
+++ sample.js
@@ -0,0 +1,31 @@
+function showVideo(){
+   document.getElementById('video').nextElementSibling.style.display="block";
+    document.getElementById('video').style.display="none";
+var x=document.getElementById('iframe');
+x.src+= "?autoplay=1";
+}
+
+function showVideo1(){
+    document.getElementById('video2').nextElementSibling.style.display="block";
+     document.getElementById('video2').style.display="none";
+ var y=document.getElementById('iframes');
+ y.src+= "?autoplay=1";
+ }
+ function showVideo3(){
+    document.getElementById('video3').nextElementSibling.style.display="block";
+     document.getElementById('video3').style.display="none";
+ var z=document.getElementById('iframe3');
+ z.src+= "?autoplay=1";
+ }
+ function showVideo4(){
+    document.getElementById('video4').nextElementSibling.style.display="block";
+     document.getElementById('video4').style.display="none";
+ var z=document.getElementById('iframe4');
+ z.src+= "?autoplay=1";
+ }
+ function showVideo5(){
+    document.getElementById('video5').nextElementSibling.style.display="block";
+     document.getElementById('video5').style.display="none";
+ var z=document.getElementById('iframe5');
+ z.src+= "?autoplay=1";
+ }
